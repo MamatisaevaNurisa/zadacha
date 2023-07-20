3425b06c91ae912bdc1ed627bb47646236e6a1cc
method <mamatisaevanurisa@icloud.com>
2023-07-20 15:50:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String s=scanner.nextLine();
+        if (s.equals("kandai")){
+            System.out.println("jakshy ozun");
+        }
+        String s1=scanner.nextLine();
+        if (s1.equals("jakshy rahmat")){
+            System.out.println("emne jkyltasyn");
+        }
+    }
+}
